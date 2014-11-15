59d4d9b19db4683715dc5783e5a25f7dd222db98
Jamie <jamie.yung286@gmail.com>
2014-11-15 18:19:44 -0500

    Added LayoutHelper.cpp

---
+++ Classes/LayoutHelper.cpp
@@ -0,0 +1,39 @@
+//
+//  LayoutHelper.cpp
+//  ChessExplorer
+//
+//  Created by Jamie Yung on 28/07/2014.
+//
+//
+
+#include "LayoutHelper.h"
+
+float LayoutHelper::getTitleFontSize() {
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    float ret = visibleSize.height/24;
+    return ret;
+}
+
+float LayoutHelper::getNormalFontSize() {
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    float ret = visibleSize.height/26;
+    return ret;
+}
+
+float LayoutHelper::getDialogEdgeMargin() {
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    float ret = visibleSize.height/64;
+    return ret;
+}
+
+float LayoutHelper::getDialogInnerMargin() {
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    float ret = visibleSize.height/64;
+    return ret;
+}
+
+float LayoutHelper::getButtonMargin() {
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    float ret = visibleSize.height/48;
+    return ret;
+}
